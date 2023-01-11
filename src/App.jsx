009a5dd31d0ee88b955df597267d6a667ee585db
Tom Sonvico <tom.sonvico.pro@gmail.com>
2023-01-11 08:45:34 +0100

    404

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/game" element={<Game />}></Route>
         <Route path="/result" element={<Result />}></Route>
-        <Route path="/*" element={<Home />}></Route>
+        <Route path="*" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
